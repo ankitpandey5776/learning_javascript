bf4883b28088f243253012839c20fba6d13db033
ankitpandey5776 <ankitpandey5776@gmail.com>
2025-05-02 14:22:38 +0000

    m1_variables

---
+++ 01_basics/m1_variables.js
@@ -0,0 +1,11 @@
+console.log("Ankit")
+const city= "Ranchi"
+console.log(city)
+let email= "123@email.co"
+console.log(email)
+ email= "12893@email.co"
+console.log(email)
+designation ="SDE"      //^ not preffered.
+console.log(designation)
+var phone="2386713981"
+console.log(phone)   //* use of var is no reccomended as it causes issues in scope of block
