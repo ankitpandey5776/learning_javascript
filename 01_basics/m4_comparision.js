5c7dad496cc5aa9ebeef44836e2327e1269d8ca6
ankitpandey5776 <ankitpandey5776@gmail.com>
2025-05-02 14:29:22 +0000

    m4_comparision

---
+++ 01_basics/m4_comparision.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log(3>2)
+console.log(3 == 2);
+console.log(3 != 2);
+
+console.log("2"> 2)  //* js itself converts string to number
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+//* == doesnt convert null to 0 whereas relational operators convert null to 0.
+
+console.log(undefined>0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+//* undefined is not treated as 0 or is not converted it remains 0 everywhere
