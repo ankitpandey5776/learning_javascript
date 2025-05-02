a4ef944da3e0b980fa966173c9e24a3c15453cb2
ankitpandey5776 <ankitpandey5776@gmail.com>
2025-05-02 14:23:56 +0000

    m2_variables

---
+++ 01_basics/m2_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+ alert(3+3)  //& will work in browser but not in node js
+let age =18
+let name="ANKIT"
+console.log(typeof name);
+console.log(typeof age);
+let IsEligible=true
+console.log(typeof IsEligible);
+console.log(typeof null);  //* null is of type object 
+onsole.log(typeof undefined); //* undefined is of type undefined..
