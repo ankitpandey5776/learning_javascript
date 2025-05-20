f90fe6f468c88293da6e45282d0118030146f9ce
ankitpandey5776 <ankitpandey5776@gmail.com>
2025-05-20 02:30:58 +0000

    iife

---
+++ 03_basics/m5_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('Ankit')        
+
+//*() for function definition  () for exection , so argument apasses there that is ankit
